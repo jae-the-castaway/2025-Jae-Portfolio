9f8eacacd99dc2e72a5fd4dc4493db08ad5e33e7
Jaeheon Kim <jaeheonkim95@gmail.com>
2025-08-19 11:15:19 -0500

    #FIX - footer

--- src/app/work/[category]/page.tsx
+++ src/app/work/[category]/page.tsx
@@ -28,6 +28,7 @@ export default function WorkCategoryPage() {
   const section = WORK[category];
   if (!section) {
     return (
+      //Fallback UI for non-existent category
       <div className="min-h-[50vh] flex flex-col items-center justify-center gap-4 text-center">
         <p className="text-lg text-gray-600">
           This section doesn&apos;t exist.
@@ -40,94 +41,98 @@ export default function WorkCategoryPage() {
   }
 
   return (
-    <div className="md:h-[100vh]] ">
+    <section className="relative min-h-screen md:h-screen">
       <Header />
-      <Gallery options={options}>
-        <div
-          id={category as string}
-          className="grid max-md:px-4
-  grid-cols-1 sm:grid-cols-2 gap-4
-         [--gap:16px]
-    [--col:calc((100vw-(5*var(--gap)))/6)]
-
-        md:grid-cols-[repeat(6,var(--col))]
-    md:auto-rows-[var(--col)]
-    md:h-[calc((3*var(--col))+(2*var(--gap)))]
+      <div className="">
+        <Gallery options={options}>
+          <div
+            id={category}
+            className="
+  mx-auto z-0 md:pb-24
+  grid gap-4 grid-cols-1 sm:grid-cols-2
+  justify-center
+  [--gap:16px]
+  md:[--col:min(calc((100%-(5*var(--gap)))/6),calc((100vh-168px-(2*var(--gap)))/3))]
+  md:grid-cols-[repeat(6,var(--col))]
+  md:[grid-template-rows:repeat(3,var(--col))]
 "
-        >
-          {section.media
-            .sort((a, b) => (a.sequence ?? 0) - (b.sequence ?? 0))
-            .map((img, i) => (
-              <div
-                key={i}
-                className={`${img.col} ${img.row} relative overflow-hidden shadow-lg shadow-black/20 `}
-              >
-                <Item
-                  width={img.width || 500}
-                  height={img.height || 750}
-                  content={
-                    // This is the lightbox image
-                    <Image
-                      src={img.src}
-                      alt=""
-                      width={1200}
-                      height={1200}
-                      className="w-full h-full object-cover"
-                    />
-                  }
+          >
+            {section.media
+              .sort((a, b) => (a.sequence ?? 0) - (b.sequence ?? 0))
+              .map((img, i) => (
+                <div
+                  key={i}
+                  className={`${img.col} ${img.row} relative  overflow-hidden shadow-lg shadow-black/20 `}
                 >
-                  {({ ref, open }) => (
-                    <Image
-                      src={img.src}
-                      alt={`Gallery ${i + 1}`}
-                      fill
-                      className={`w-full h-full object-cover object-[${
-                        img.objectPosition || "center"
-                      }]`}
-                      loading="lazy"
-                      ref={ref}
-                      onClick={open}
-                    />
-                  )}
-                </Item>
-              </div>
-            ))}
+                  <Item
+                    width={img.width || 500}
+                    height={img.height || 750}
+                    content={
+                      // This is the lightbox image
+                      <Image
+                        src={img.src}
+                        alt=""
+                        width={1200}
+                        height={1200}
+                        className="w-full h-full object-cover"
+                      />
+                    }
+                  >
+                    {({ ref, open }) => (
+                      <Image
+                        src={img.src}
+                        alt={`Gallery ${i + 1}`}
+                        // fill
+                        width={img.width || 500}
+                        height={img.height || 750}
+                        className={`w-full h-full object-cover object-[${
+                          img.objectPosition || "center"
+                        }]`}
+                        ref={ref}
+                        priority={i < 3}
+                        onClick={open}
+                      />
+                    )}
+                  </Item>
+                </div>
+              ))}
+          </div>
+        </Gallery>{" "}
+        <div className="md:absolute md:bottom-0 md:left-1/2 md:-translate-x-1/2 md:z-10 py-8 flex justify-center items-center text-center gap-4 text-sm text-gray-500">
+          <Link
+            href={"/work/weddings"}
+            className={`hover:text-gray-700 ${
+              category === "weddings" ? "font-bold" : ""
+            }`}
+          >
+            Wedding
+          </Link>
+          <Link
+            href={"/work/portraits"}
+            className={`hover:text-gray-700 ${
+              category === "portraits" ? "font-bold" : ""
+            }`}
+          >
+            Portrait
+          </Link>
+          <Link
+            href={"/work/products"}
+            className={`hover:text-gray-700 ${
+              category === "products" ? "font-bold" : ""
+            }`}
+          >
+            Product
+          </Link>
+          <Link
+            href={"/work/videos"}
+            className={`hover:text-gray-700 ${
+              category === "videos" ? "font-bold" : ""
+            }`}
+          >
+            Video
+          </Link>
         </div>
-      </Gallery>
-      <div className="my-8 flex h-auto justify-center items-center text-center gap-4 text-sm text-gray-500">
-        <Link
-          href={"/work/weddings"}
-          className={`hover:text-gray-700 ${
-            category === "weddings" ? "font-bold" : ""
-          }`}
-        >
-          Wedding
-        </Link>
-        <Link
-          href={"/work/portraits"}
-          className={`hover:text-gray-700 ${
-            category === "portraits" ? "font-bold" : ""
-          }`}
-        >
-          Portrait
-        </Link>
-        <Link
-          href={"/work/products"}
-          className={`hover:text-gray-700 ${
-            category === "products" ? "font-bold" : ""
-          }`}
-        >
-          Product
-        </Link>
-        <Link
-          href={"/work/videos"}
-          className={`hover:text-gray-700 ${
-            category === "videos" ? "font-bold" : ""
-          }`}
-        >
-          Video
-        </Link>
       </div>
-    </div>
+    </section>
   );
 }
